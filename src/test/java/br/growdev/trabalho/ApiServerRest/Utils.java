5c27979ab745406aa1d6e1d69aa6098e98ac0f5a
Amilton Jose <amiltonjosefn2019@gmail.com>
2023-04-11 18:49:45 -0300

    commit refatoração do metodo pegaToken

--- src/test/java/br/growdev/trabalho/ApiServerRest/Utils.java
+++ src/test/java/br/growdev/trabalho/ApiServerRest/Utils.java
@@ -1,5 +1,6 @@
 package br.growdev.trabalho.ApiServerRest;
 
+import br.growdev.trabalho.ApiServerRest.dominio.Login;
 import br.growdev.trabalho.ApiServerRest.dominio.Usuario;
 import br.growdev.trabalho.ApiServerRest.teste.TesteBase;
 import com.github.javafaker.Faker;
@@ -52,18 +53,48 @@ public class Utils extends TesteBase {
 
 
     public static String pegaToken() {
-        Map<String, String> usuario = new HashMap<>();
-        usuario.put("email", "giulia.jaques@yahoo.com");
-        usuario.put("password", "3fobtl6mok7");
-
+        Login loginValido = retornaLoginValido();
         return given()
-                    .contentType(ContentType.JSON)
-                    .body(usuario)
+                .contentType(ContentType.JSON)
+                .body(loginValido)
                 .when()
-                    .post(LOGIN_ENDPOINT)
+                .post(LOGIN_ENDPOINT)
                 .then()
-                    .contentType(ContentType.JSON)
-                    .extract().path(AUTENTICACAO);
+                .log().all()
+                .contentType(ContentType.JSON)
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", is("Login realizado com sucesso"))
+                .extract().path(AUTENTICACAO);
+    }
+
+    private static Login retornaLoginValido() {
+        Faker faker = new Faker(new Locale("pt-br"));
+
+        Usuario criaUsuario = new Usuario();
+        criaUsuario.setNome(faker.address().firstName());
+        criaUsuario.setEmail(faker.internet().emailAddress());
+        criaUsuario.setPassword(faker.internet().password());
+        criaUsuario.setAdministrador("true");
+
+        String id = given()
+                .contentType(ContentType.JSON)
+                .body(criaUsuario)
+                .when()
+                .post(CADASTRA_USUARIO_ENDPOINT)
+                .then()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .statusCode(HttpStatus.SC_CREATED)
+                .extract().path("_id");
+
+        return  when()
+                .get(BUSCA_USUARIO_PORID_ENDPOINT+id)
+                .then()
+                .log().all()
+                .extract()
+                .as(Login.class);
+
+
     }
 
 }
